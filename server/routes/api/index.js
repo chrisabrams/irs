872dc45fb7b030e6603f2b07ff4863eb8f90df07
Chris Abrams <mail@chrisabrams.com>
2014-10-10 11:54:36 -0400

    Pipe sharp'd image to browser.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,7 @@
 var fs    = require('fs'),
     http  = require('http'),
     path  = require('path'),
+    request = require('request'),
     sharp = require('sharp')
 
 module.exports = function(server) {
@@ -12,27 +13,15 @@ module.exports = function(server) {
     // Starting with writing a .jpg
     var outputFile = fs.createWriteStream(path.join(__dirname, '/../../../test/output/file.jpg'))
 
-    // Get the image that needs to be resized
-    var get = http.get(imagePath, function(response) {
+    var transformer = sharp().resize(640,480).crop(sharp.gravity.north)
 
-      var transformer = sharp().resize(640,480).crop(sharp.gravity.north)
+    req
+      .pipe(request(imagePath)) // Get the image requested
+      .pipe(transformer)        // Apply sharp settings
+      .pipe(res)                // Send data to browser
 
-      response
-        .pipe(transformer) // Apply sharp settings
-        .pipe(outputFile)  // Write the output of the final image to disk
-
-      //req.pipe(response.pipe(transformer)) <-- not working
-
-    })
-
-    get.on('data', function(data) {
-      console.log("got data")
-      res.write(data)
-    })
-
-    get.on('end', function() {
-      res.end()
-    })
+    /* cant seem to split a stream into two pipes :/ */  
+      //.pipe(outputFile)         // Write the output of the final image to disk
 
   })
 
